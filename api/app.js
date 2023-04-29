b986d97578e6be72e141400b4780315d49bab416
Marcelo Souza Santos <marcelo.santos@virtus.ufcg.edu.br>
2023-04-29 16:14:23 -0300

    teste - teste do servidor

--- api/app.js
+++ api/app.js
@@ -1,22 +1,21 @@
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
-
-const port = process.env.PORT || 3000;
-const router = require('./routes/book.rootes');
-
+const router = require("./routes/book.rootes");
+const https = require(‘https’);
+const fs = require(‘fs’);
+const options = {
+    key: fs.readFileSync(‘/etc/letsencrypt/live/$DOMAIN/privkey.pem’),
+cert: fs.readFileSync(‘/etc/letsencrypt/live/$DOMAIN/fullchain.pem’)
+};
 const app = express();
-
 app.use(morgan('tiny'));
 app.use(helmet());
 
 app.use('/api/v1', router);
-
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.use((req, res) =>
+{
+    res.end(‘Hello World’);
 });
 
-app.use((req, res) => {
-    res.status(404).send('It is not found.');
-});
+https.createServer(options, app).listen(8000);
