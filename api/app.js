00fb12f137f52b068d71b0fec6fcc2dba5fda69d
Marcelo Souza Santos <marcelo.santos@virtus.ufcg.edu.br>
2023-04-29 16:16:22 -0300

    teste - ajustando

--- api/app.js
+++ api/app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const router = require("./routes/book.rootes");
-const https = require(‘https’);
-const fs = require(‘fs’);
+const https = require('https');
+const fs = require('fs');
 const options = {
-    key: fs.readFileSync(‘/etc/letsencrypt/live/$DOMAIN/privkey.pem’),
-cert: fs.readFileSync(‘/etc/letsencrypt/live/$DOMAIN/fullchain.pem’)
+    key: fs.readFileSync('/etc/letsencrypt/live/$DOMAIN/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/$DOMAIN/fullchain.pem')
 };
 const app = express();
 app.use(morgan('tiny'));
@@ -15,7 +15,7 @@ app.use(helmet());
 app.use('/api/v1', router);
 app.use((req, res) =>
 {
-    res.end(‘Hello World’);
+    res.end('Hello World');
 });
 
 https.createServer(options, app).listen(8000);
