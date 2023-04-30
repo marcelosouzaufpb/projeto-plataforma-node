dc6b478c354d820f5b3863df83d5b28361cbd29a
Marcelo Souza Santos <marcelo.santos@virtus.ufcg.edu.br>
2023-04-30 11:25:19 -0300

    atividade um

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import {PrismaClient} from '@prisma/client';
 import fastify from 'fastify'
-import {z} from 'zod';
 
 const app = fastify();
 const prisma = new PrismaClient();
@@ -9,58 +8,93 @@ const version = 'v1';
 const path = 'books';
 const url = `/api/${version}/${path}`
 
-app.get(url, async () => {
-    const books = await prisma.book.findMany();
+let bookstore: any = {
+    books: [
+        {id: 1, title: 'teste', author: 'testador'}
+    ]
+};
 
-    return {books};
+app.get(url, async () => {
+    // const books = await prisma.book.findMany();
+    //
+    // return {books};
+
+    return bookstore;
 });
 
 app.get(`${url}/:id`, async (request: any, reply: any) => {
     const id = String(request?.params?.id);
-    const item = await prisma.book.findUnique({
-        where: {id: id}
-    });
+    // const item = await prisma.book.findUnique({
+    //     where: {id: id}
+    // });
+
+    const item = bookstore.books.find((item: any) => item.id == id);
+    if (!!item) {
+        return reply.status(404);
+    }
+
     return reply.status(200).send(item);
 });
 
-app.post(url, async (request, reply) => {
-    const validationSchema = z.object({
-        title: z.string(),
-        author: z.string()
-    });
+app.post(url, async (request: any, reply: any) => {
+    // const validationSchema = z.object({
+    //     title: z.string(),
+    //     author: z.string()
+    // });
+    //
+    // const {title, author} = validationSchema.parse(request.body);
+    //
+    // const item: any = await prisma.book.create({
+    //     data: {title, author}
+    // });
+    //
+    // return reply.status(201).send(item);
 
-    const {title, author} = validationSchema.parse(request.body);
+    request.body.id = bookstore.books.length + 1;
+    bookstore.books.push(request.body);
 
-    const item: any = await prisma.book.create({
-        data: {title, author}
-    });
-
-    return reply.status(201).send(item);
+    return reply.status(201).send(request.body);
 });
 
-app.put(`${url}/:id`, async (request: any, reply) => {
+app.put(`${url}/:id`, async (request: any, reply: any) => {
     const id = String(request?.params?.id);
-    const validationSchema = z.object({
-        title: z.string(),
-        author: z.string()
+    // const validationSchema = z.object({
+    //     title: z.string(),
+    //     author: z.string()
+    // });
+    //
+    // const {title, author} = validationSchema.parse(request.body);
+    //
+    // const item: any = await prisma.book.update({
+    //     where: {id: id},
+    //     data: {title, author}
+    // });
+
+    let thereIs: boolean = false;
+    let currentItem: any = null;
+    bookstore.books = bookstore.books.map((item: any) => {
+        if (item.id === id) {
+            item = request.body
+            thereIs = true;
+            currentItem = item;
+        }
+        return item;
     });
 
-    const {title, author} = validationSchema.parse(request.body);
-
-    const item: any = await prisma.book.update({
-        where: {id: id},
-        data: {title, author}
-    });
-
-    return reply.status(200).send(item);
+    return !thereIs
+        ? reply.status(404)
+        : reply.status(200).send(currentItem);
 });
 
-app.delete(`${url}/:id`, async (request: any, reply) => {
+app.delete(`${url}/:id`, async (request: any, reply: any) => {
     const id = String(request?.params?.id);
-    const item: any = await prisma.book.delete({
-        where: {id: id}
-    });
-    return reply.status(200).send(item);
+    // const item: any = await prisma.book.delete({
+    //     where: {id: id}
+    // });
+
+    bookstore.books = bookstore.books.filter((item: any) => item.id != id);
+
+    return reply.status(200).send(bookstore);
 });
 
 
